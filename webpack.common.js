b8fe378ddb9564a00dce8f8e0a21dfc722a4a3ed
Miaomiao Li <ljsabc@gmail.com>
2021-05-28 17:20:13 +0800

    Fix configs and vulnerabilities.

--- webpack.common.js
+++ webpack.common.js
@@ -38,7 +38,12 @@ module.exports = {
     {
       test: /\.css$/,
       use: [MiniCssExtractPlugin.loader,
-        'css-loader'
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        }
       ]
 
     },
